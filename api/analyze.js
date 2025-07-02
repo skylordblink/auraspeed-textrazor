0bfe6726ead7e26cd2d9c2b037f7e7355621cf7c
skylordblink <134183499+skylordblink@users.noreply.github.com>
2025-07-02 12:07:07 -0400

    analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
       },
       body: new URLSearchParams({
         text,
-        extractors: "entities,topics,sentiment",
+        extractors: "entities",
       }),
     });
 
